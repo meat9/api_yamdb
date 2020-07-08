2b26bd37a8d395b75d254fa43a343ab10c465534
afrlv1 <afrlv@yandex.ru>
2020-07-08 17:53:14 +0500

    Modernized urls

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -7,9 +8,12 @@ from rest_framework_simplejwt.views import (
         TokenObtainPairView,
         TokenRefreshView,
     )
+
+from api_.views import CategoryViewSet, TitleViewSet, GenreViewSet
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api_user.urls')),
-    path('api/v1/', include('api_titles.urls')),
+    path('api/v2/', include('api_titles.urls')),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
 ]
