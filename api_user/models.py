c1d3950d99b3eade94eef3946a65391724a9bfd1
meat9 <meat9@yandex.ru>
2020-07-08 06:51:18 +0300

    token don work

--- api_user/models.py
+++ api_user/models.py
@@ -6,13 +6,13 @@ class CustomSuperUser(BaseUserManager):
     
     def create_user(self, email, password=None, **kwargs):
         user = self.model(email=email, **kwargs)
-        user.set_password(password)
+        user.set_password(validated_data['password'])
         user.save()
         return user
 
     def create_superuser(self, email, password, **kwargs):
         user = self.model(email=email, is_staff=True, is_superuser=True, **kwargs)
-        user.set_password(password)
+        user.set_password(validated_data['password'])
         user.save()
         return user
 
