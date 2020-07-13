0004e904641e605a90807a2a3d57236dc9ba3d91
afrlv1 <afrlv@yandex.ru>
2020-07-13 11:03:44 +0500

    Modernized urls

--- api_titles/views.py
+++ api_titles/views.py
@@ -1,20 +1,19 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, filters, status, permissions
+from rest_framework import viewsets, filters, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from .models import Category, Genre, Title
 from .serializers import CategorySerializer, TitleSerializer, GenreSerializer
 from .filters import TitlesFilter
-from api_user.permissions import AdmPerm as IsAdminOrReadOnly
+from api_user.permissions import AdmPerm
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = PageNumberPagination
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [AdmPerm]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
     lookup_field = 'slug'
@@ -30,7 +29,7 @@ class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = PageNumberPagination
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [AdmPerm]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
     lookup_field = 'slug'
@@ -46,6 +45,6 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     pagination_class = PageNumberPagination
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [AdmPerm]
     filter_backends = [DjangoFilterBackend]
     filterset_class = TitlesFilter
--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-        TokenObtainPairView,
-        TokenRefreshView,
-    )
 
-from api_.views import CategoryViewSet, TitleViewSet, GenreViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api_user.urls')),
-    path('api/v2/', include('api_titles.urls')),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
 ]
