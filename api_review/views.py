bee952bd429540df966614eeef494ca116b337ba
liliya-ily <lylyshaunique@gmail.com>
2020-07-16 16:44:46 +0300

    Nihrena ne poluchaetsa

--- api_review/views.py
+++ api_review/views.py
@@ -16,6 +16,7 @@ from rest_framework.decorators import api_view
 class ReviewViewSet(viewsets.ModelViewSet): 
     queryset = Review.objects.all() 
     serializer_class = ReviewSerializer
+
     def get_permissions(self):
         permission_classes = []
         if self.action == 'list':
@@ -26,30 +27,27 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        if Review.objects.filter(author=self.request.user).exists():
-            raise ValidationError('Оценка уже выставлена')
-        if serializer.is_valid(): 
-            serializer.save(author=self.request.user, title=title)            
-            return Response(serializer.data, status=status.HTTP_201_CREATED) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @api_view(['GET', 'POST', 'PUT']) 
-    def api_review(self, request):
         if not self.request.auth:
             return Response(status=status.HTTP_404_FORBIDDEN)
-        if request.method == 'GET':
+        
+        if Review.objects.filter(author=self.request.user).exists():
+            raise ValidationError('Оценка уже выставлена')
+
+        if self.request.method == 'GET':
             reviews = Review.objects.all()
             serializer = ReviewSerializer(reviews, many=True)
             return Response(serializer.data)
-        elif request.method == 'POST'or request.method == 'PUT':            
-            title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))       
-            if self.request.data.score not in range (1, 10):
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            serializer = ReviewSerializer(data=request.data) 
+        elif self.request.method == 'POST' or self.request.method == 'PUT':  
             if serializer.is_valid(): 
                 serializer.save(author=self.request.user, title=title)
                 return Response(serializer.data, status=status.HTTP_201_CREATED) 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+        if serializer.is_valid(): 
+            serializer.save(author=self.request.user, title=title)    
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
