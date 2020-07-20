476ae7af45e836e974e3315c400e7565cf1a2114
meat9 <61844244+meat9@users.noreply.github.com>
2020-07-20 21:37:12 +0300

    Update urls.py

--- api_user/urls.py
+++ api_user/urls.py
@@ -8,7 +8,7 @@ from api_review.views import ReviewViewSet, CommentViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'users/', UserViewSet, basename='users')
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'categories/', CategoryViewSet)
 router.register(r'genres/', GenreViewSet)
 router.register(r'titles/', TitleViewSet)
